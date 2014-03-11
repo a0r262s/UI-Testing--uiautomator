aaf143b81fd0a74340a1851b96e79e0ad39e4880
a0r262s <a0r262s@gmail.com>
2014-03-11 23:41:05 +0100

    Upload

---
+++ SkeletonApp2/app/src/main/gen/R.java.d
@@ -0,0 +1,7 @@
+C:\Users\a0r262s\AndroidStudioProjects\SkeletonApp2\app\src\main\gen\com\example\android\skeletonapp\R.java \
+ : C:\Users\a0r262s\AndroidStudioProjects\SkeletonApp2\app\src\main\res\drawable\violet.jpg \
+C:\Users\a0r262s\AndroidStudioProjects\SkeletonApp2\app\src\main\res\layout\skeleton_activity.xml \
+C:\Users\a0r262s\AndroidStudioProjects\SkeletonApp2\app\src\main\res\values\colors.xml \
+C:\Users\a0r262s\AndroidStudioProjects\SkeletonApp2\app\src\main\res\values\strings.xml \
+C:\Users\a0r262s\AndroidStudioProjects\SkeletonApp2\app\src\main\res\values\styles.xml \
+C:\Users\a0r262s\AndroidStudioProjects\SkeletonApp2\app\src\main\bin\AndroidManifest.xml \
